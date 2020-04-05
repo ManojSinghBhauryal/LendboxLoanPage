f01c9c546873633bf1f98376859ca59a2b5a2a36
ManojSinghBhauryal <manojmanoj598@gmail.com>
2020-04-06 00:04:58 +0530

    commit

---
+++ pages/index.vue
@@ -0,0 +1,264 @@
+<template>
+  <div class="container">
+    <div class="top">
+   <header>
+     <h1>Get Instant Personal Loan Online With Lendbox</h1>
+     <p>Register and submit documents online. Interest rates starting from 11.49%. Quick Registration.</p>
+     <p>Avail instant personal loans ranging from Rs. 50,000 to 10 Lakhs, not on a long term or short term but on your own terms. Experience a completely online and paperless process. Check your eligibility, Calculate EMI and Apply now!</p>
+     <button>Apply here</button>
+     <div class="links">
+       <b>Jump To:</b>
+       <a href="">Calculate Your EMI</a> |
+       <a href="">Features</a> |
+       <a href="">Eligibility</a> |
+       <a href="">Documents</a> |
+       <a href="">Repayment & Fees</a>
+     </div>
+   </header>
+   </div>
+   <section>
+     <h2>What is a personal loan and why you should take a instant loan from Lendbox?</h2><br>
+
+     <h3>What is a personal Loan?</h3>
+     <p>An <b>Instant Personal Loan</b> or an Instant loan is a form of loan taken from a lender or a financial institution for personal uses such as family vacations, medical needs, consumer electronics and appliances, <a href="" style="color:#1458EA">debt consolidation</a>, credit card bill consolidation, wedding or any other such needs.</p>
+     <p>In a personal loan, lenders lend money to borrower at a fixed or floating interest rate for a certain period of time which can range from 3 months to 3 years or even more depending on the amount without providing any collateral or asset as security or a guarantor. After that borrower repays the loan amount with easy monthly installments.</p>   
+     <p>You can negotiate the interest rate in some forms of lending such as <b>peer-to-peer lending</b>.</p>
+
+     <h3>What is Lendbox?</h3>
+     <p>Lendbox is an online peer-to-peer lending platform that helps individual borrowers connect with vast base of institutional and individual lenders for funding their loan instantly on a negotiated interest rate.</p>
+     <p>Lendbox also provide instant loans through major banks and NBFCs, so we have you covered, if you slightly misses the eligibility criteria for banks and NBFCs, you can still get a personal loan through our individual lenders. Our credit assessment is much more flexible and relies on almost 300 data points not just your CIBIL score.</p>
+     
+     <h3>Benefits of taking an Instant Personal Loan from Lendbox</h3><br>
+     <img src="@/assets/instant-personal-loan-online.png"/>
+     <p>With an approval rate of 45% and interest rates starting from just 12% per annum, Lendbox has served over 50,000 within a period of one year. Apart from that, below are some of the major features that make Lendbox best place to take loan in India.</p>
+     <h4>High Approval Rate</h4>
+     <p>As we mentioned earlier, the approval rate for personal loan applications in Lendbox is over 45%. We have been able to serve a larger sector of people than that of banks because of our big data analytics engine and our better ability to understand customer profiles, their need, intent and ability to repay.</p>
+     <h4>Low Interest Rates</h4>
+     <p>Interest rate of a personal loan on Lendbox starts from 12% per annum. Not much different than banks? Well, here's the catch, there are no hidden charges. Pay small processing fees and that too only at disbursement time plus there no charges for pre-closure so you can close your loan before the loan tenure without any extra charges.</p>
+     <h4>Instant Approval</h4>
+     <p>After submitting your documents, Lendbox will assess your profile and approve your loan within hours you can get your personal loan in an instant. With <a href="" style="color:#1458EA">Short term loan</a>, you can get the amount disbursed within 24 hours</p>
+     <p>(<b>Note</b>: Personal loans from banks and NBFCs will have high pre closure charges as per their standards; you can find that info further below in the page)</p>
+     <h4>Pan India Coverage</h4>
+     <p>Lendbox operates in over 35 cities in India, which covers almost all big Tier I and Tier II cities. We can provide you a personal loan whether you're in Delhi, Bangalore, Hyderabad, Chennai, and Mumbai or anywhere else.</p>
+
+     <h2>How To Apply For A Instant Personal Loan Online</h2>
+     <p>It's very easy to apply for a personal loan at Lendbox. it's a completely online, paperless and hassle free process. To apply, follow the steps given below.</p>
+     <div>
+       <img src = "@/assets/peer-to-peer-loans.png"/>
+     </div>
+     <p>Before applying, make sure you are eligible for an instant loan and have enough documents to get approved. You can check eligibility criteria and required documents below in the post. Keep the documents with yourself during the time of application for a quick approval. Make sure your Internet is stable.</p>
+     <b>Now, follow these steps:-</b>
+     <p>
+       <ul>
+        <li>Go to or open the https://app.lendbox.in/signup/borrower in a new tab of browser or click on this link.</li>
+        <li>Fill out all the details as per your KYC Documents</li>
+        <li>Pay a small registration fee of Rs. 500</li>
+        <li>Upload the copies of all the required documents</li>
+        <li>Complete all the steps and you're done</li>
+      </ul>
+     </p>
+     <p>After completing the process, we'll contact you with the further details. Here is a little video to help you out with the whole application process.</p>
+     <p>If you're creditworthy enough to get a loan, you'll get an activation mail or a call from our representatives in a matter of minutes. Sometimes it can take up to 24 hours in activation and approval. Once activated, you can start operating on the platform.</p>
+     
+     <h2>Personal Loan Interest Rates</h2>
+     <p>The interest rate for personal loans in Lendbox starts from the min 11.49% p.a. and can go upto 36%, depending on the credit rating of the borrower. The credit ratings are given after the assessment of the borrower.</p>
+     <p>Lendbox looks beyond CIBIL score and salary to assess the creditworthiness of the borrowers. Our credit assessment relies on over 250 parameters and big data analytics, making sure that lenders feel safe to invest and provide personal loan to applicants.</p>
+     <p>If you have a good solid credit history and high salary then you can avail a personal loan at the lowest interest rates within minutes. Also, having small loan tenure helps you in keeping the interest amount low. Below is an example of how the interest rate affects the cost of a personal loan and its EMI's.</p>
+     <p><b>Example</b> - A Personal loan for Rs. 2,00,000 at an interest rate of 12% with a repayment period of 12 months will have a monthly EMI's of Rs. 17,769 and the total payable interest amount will be Rs. 13,237. The amount you'll repay in 12 months will be Rs. 2,13,237.</p>
+     
+     <h3>Here Is An Example Table With Different Interest Rates:</h3>
+     <table>
+       <tr>
+         <td>Amount</td>
+         <td>Interest Rate</td>
+         <td>Tenure</td>
+         <td>EMI to pay</td>
+       </tr>
+       <tr>
+         <td>1,00,000</td>
+         <td>12% p.a.</td>
+         <td>12 Months</td>
+         <td>8,884</td>
+       </tr>
+       <tr>
+         <td>1,00,000</td>
+         <td>15% p.a.</td>
+         <td>12 Months</td>
+         <td>9,025</td>
+       </tr>
+       <tr>
+         <td>1,00,000</td>
+         <td>20% p.a.</td>
+         <td>12 Months</td>
+         <td>9,263</td>
+       </tr>
+       <tr>
+         <td>1,00,000</td>
+         <td>25% p.a.</td>
+         <td>12 Months</td>
+         <td>9,504</td>
+       </tr>
+     </table>
+     <p>You can calculate your Personal loan EMI by yourself with our EMI Calculator</p>
+
+      <!-- EMI calculator -->
+
+     <h2>Eligibility & Required Documents</h2>
+     <p>Before applying for a personal loan, you need to make sure that you're eligible for it and have all the required documents so you don't get stuck after.</p>
+     <p>Different financial institutions follow different eligibility criteria, below is the eligibility criteria for Lendbox.</p>
+     <p>
+       <ul>
+        <li>You must be Indian resident</li>
+        <li>Minimum eligible age is 21 years</li>
+        <li>You must have an Indian bank account</li>
+        <li>You must a valid India ID proof</li>
+        <li>Your monthly net income should be at least 25000</li>
+       </ul>
+     </p>
+     <p>If you meet all the above mentioned criteria's then you are eligible enough to apply for a personal loan in Lendbox. Once you applied, you'll have to upload copies of the following documents for verification and credit assessment.</p>
+     <b>Note: Documents with (*) are mandatory.</b>
+     <table>
+       <tr>
+         <td>Photographs</td>
+         <td>One</td>
+       </tr>
+       <tr>
+         <td>Identity Proof (any one)</td>
+         <td>PAN Card*, aadhaar Card, Voter ID*, Passport</td>
+       </tr>
+       <tr>
+         <td>DOB Proof (any one)</td>
+         <td>PAN Card*, Signature verification from bank, Passport</td>
+       </tr>
+       <tr>
+         <td>Address Proof</td>
+         <td>	Passport, Bank Statement, Voter ID*, Aadhar Card, Utility Bill (Electricity/Mobile/Landline), Rent Agreement</td>
+       </tr>
+       <tr>
+         <td>Bank Statement</td>
+         <td>Last 6 Months</td>
+       </tr>
+       <tr>
+         <td>Income Proof Proof (any one)</td>
+         <td>Last year ITR (Income Tax Return) or Income Declaration</td>
+       </tr>
+     </table>
+     <br>
+     <br>
+     <h2>Personal Loan Processing Fees & Repayment</h2>
+     <p>You can avail a personal loan at repayment tenure of min 3 months to a max 60 months. With a little registration fees of Rs.500, Lendbox gives every borrower a chance to try, Once a borrower gets fully funded, he'll have to pay a minimal 2-6% processing fees for initiating disbursement of his personal loan.</p>
+     <p>Once the loan has been disbursed, borrower will have to start paying EMI's from the next month. You'll have to pay EMI's between 1 to 10th of each month. If the borrower fails to pay any EMI by the due date, he'll have to pay a bounce charge and additional penal interest of 2% p.m. on the next payment.</p>
+     <p>There are no pre-payment or pre-closure charges, if a borrower wants to close his loan before the actual tenure, then he can do it without any additional charges.</p>
+     <h3>Cancellation & Refunds</h3>
+     <p>Lendbox reserves the right to disapprove or reject any application if the applicant is not suitable to get a personal loan from the platform or our financing partners. Lendbox can also terminate any approved application if any issue arises later.</p>
+     <p>In case, any applicant has paid any fees prior to activation or approval, the refund policy goes as given below.</p>
+     <ul>
+       <li>If Lendbox terminates an account, we will refund Rs. 1000 and a credit analysis report and reasons for rejection will be provided.</li>
+       <li>If a borrower fails to get funded and decides to close the application then a refund of Rs. 500 will be made.</li>
+     </ul>
+     <p>Still need to know more? Have a look at the FAQ for personal loans and peer to peer lending below.</p>
+
+      <!-- Customer review -->
+
+     <h2>Frequently Asked Questions For Personal Loans & P2P Lending</h2>
+     <p><b>Q. How Much I Can Borrow?</b></p>
+     <p>You can borrow anywhere between Rs. 50,000 to Rs. 10 Lakhs. It is also depends on your credit rating; a capping on amount is possible based on your credit analysis.</p>
+      <p><b>Q. Are there any hidden charges?</b></p>
+      <p>No, Lendbox is all about transparency and all the activities on the website can be monitored by the borrower.</p>
+      <p><b>Q. How Do Lendbox Check My Credit Worthiness?</b></p>
+      <p>We do credit analysis based on your inputs and received documents. Some of the things which majorly affect the creditworthiness of a borrower are :</p>
+      <p>
+        <ul>
+          <li>Net Monthly Salary</li>
+          <li>Monthly Expenses</li>
+          <li>Loan Purpose</li>
+          <li>Current Employment Status</li>
+          <li>Work Experience</li>
+          <li>Your Location</li>
+        </ul>
+      </p>
+      <p><b>Q. What Is Peer To Peer Lending?</b></p>
+      <p>Peer to peer lending or p2p lending is a process in which a lender directly lends money to a borrower at a negotiated interest rate on an online or offline platform.</p>
+      <p>To know more about peer to peer lending, Go to - Peer to Peer Lending Explained</p>
+      <p><b>Q. How Will I Get The Amount?</b></p>
+      <p>You will get the disbursed amount in your bank account.</p>
+      <p>Have more questions? Check more Personal Loan FAQ's here or send a mail to support@lendbox.in for any particular query. You can find our contact numbers also in the footer.</p>
+   </section>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+.top{
+  background-color: #1458EA;
+}
+header{
+  height:700px;
+  color:white;
+  text-align: center;
+  padding:110px 10px 10px 10px;
+}
+p{
+  margin:15px;
+}
+button{
+  margin:15px;
+  background-color: white;
+  color:#1458EA;
+  padding:10px;
+  border:none;
+  cursor: pointer;
+  font-weight:bold;
+  border-radius: 5px;
+}
+.links a{
+  color:white;
+
+}
+section{
+  padding:10px;
+  padding-top:100px;
+  margin-top:20px;
+  text-align: justify;
+  text-justify: inter-word;
+}
+section img{
+  width:350px;
+}
+table{
+  border-collapse: collapse;
+  width: 100%;
+}
+table td, table td{
+  border-bottom: 1px solid #ddd;
+  padding: 8px;
+}
+table tr:nth-child(odd){
+  background-color:#F9F9F9;
+}
+
+@media only screen and (min-width: 768px){
+  header{
+    height:500px;
+    margin:auto;
+    width:900px;
+    padding:120px;
+  }
+  section{
+    margin:auto;
+    width:950px;
+    padding:60px 90px 90px 90px;
+  }
+  section img{
+  width:100%;
+}
+}
+
+</style>
